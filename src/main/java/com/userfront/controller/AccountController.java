1902082808ae32c155da67eab1c695478e579f39
Volodymyr <rozdolskyvolodymyr@gmail.com>
2019-02-28 12:22:20 -0500

    adding primaryAccount

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -1,19 +1,45 @@
 package com.userfront.controller;
 
+import com.userfront.domain.*;
+import com.userfront.service.AccountService;
+import com.userfront.service.UserService;
+import org.omg.IOP.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
 
-    @RequestMapping("primaryAccount")
-    private String primaryAccount(){
+    @Autowired
+    private UserService userService;
+
+
+
+    @RequestMapping("/primaryAccount")
+    public String primaryAccount(Model model, Principal principal) {
+
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+
         return "primaryAccount";
     }
-    @RequestMapping("savingsAccount")
-    private String savingsAccount(){
+
+    @RequestMapping("/savingsAccount")
+    public String savingsAccount(Model model, Principal principal) {
+
+        User user = userService.findByUsername(principal.getName());
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("savingsAccount", savingsAccount);
+
         return "savingsAccount";
     }
-
 }
