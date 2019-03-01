dcb859405d68803e9c251168c06db83376f70886
Volodymyr <rozdolskyvolodymyr@gmail.com>
2019-03-01 17:30:20 -0500

    appointment

---
+++ src/main/java/com/userfront/dao/AppointmentDao.java
@@ -0,0 +1,14 @@
+package com.userfront.dao;
+
+import com.userfront.domain.Appointment;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface AppointmentDao extends CrudRepository<Appointment, Long> {
+
+    List<Appointment> findAll();
+
+//    Appointment findOne(Long id);
+}
